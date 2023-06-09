a77790eea4e4e05158ff1d3c34fb76c7583e8a96
Your Name <you@example.com>
2023-06-09 17:33:21 +0530

    basic mapper added

---
+++ src/main/java/com/example/mapstructlearning/dto/ContactDTO.java
@@ -0,0 +1,13 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class ContactDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNo;
+}
