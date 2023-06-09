9135ec360b3a2d8410dbb10916c5b7e887f4acf9
Your Name <you@example.com>
2023-06-09 16:22:25 +0530

    first commit

---
+++ src/main/java/com/example/mapstructlearning/MapStructLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapstructlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStructLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapStructLearningApplication.class, args);
+	}
+
+}
